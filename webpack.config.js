bd8c5d60fabf09787c4663aeeb7b8000d7a50c1b
Nikola Mitic <nikola.mitic@namics.com>
2018-01-30 22:52:55 +0100

    return dettool to source-map

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
+  devtool: 'source-map',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'svyte.js',
